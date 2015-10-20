9a11e100b1c098b62ef45c13ff645eec843cb87f
smarbos <smarbos@gmail.com>
2015-10-20 16:30:50 -0300

    Remove mesa and mesas controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -148,28 +148,4 @@ console.log($scope.menu);
       $scope.closeLogin();
     }, 1000);
   };
-})
-
-.controller('AddTable', function($scope, $ionicModal) {
-
-})
-
-.controller('MesasCtrl', function($scope, $ionicModal, TablesService) {
-  $scope.mesas = TablesService.getTables();
-})
-
-.controller('MesaCtrl', function($scope, TablesService, $location) {
-  var mesaId = $location.path().split("/")[3]||"Unknown";
-  TablesService.setCurrentTable(mesaId);
-  $scope.mesa = TablesService.getTable(mesaId-1)
-  $scope.changeStatus = function(tableId, newStatus){
-    $scope.mesas = TablesService.getTables();
-    var match = _.find($scope.mesas, function(table) { return table.id === tableId })
-    if (match) {
-        match.status = newStatus;
-    }
-  }
-
-
-
 });
